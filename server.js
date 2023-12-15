a9930fa74ad444066a39d70e9d6ee7f40463250f
Felisya <felisyawong21@gmail.com>
2023-12-15 13:26:47 +0800

    initial commit

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use('/api/incomes', require('./routes/incomeRoutes'))
 
 app.use(errorHandler)
 
-// app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
